cfbc6e4605b2dfe6e3854f67e0ef401a5ac53670
Minchuk Ilya <cacatubutyt@gmail.com>
2023-12-08 02:38:08 +0100

    feat: add Footer in RootLayout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import classes from './RootLayout.module.css';
 import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 function RootLayout() {
   return (
@@ -9,7 +10,7 @@ function RootLayout() {
       <main className={classes.main}>
         <Outlet />
       </main>
-      <footer>footer</footer>
+      <Footer>footer</Footer>
     </div>
   );
 }
