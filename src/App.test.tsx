80ae22b1f9b12f3e3c0132da726e55a43bf71542
Minchuk Ilya <cacatubutyt@gmail.com>
2023-12-08 11:12:58 +0100

    refactor: remove useless test

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-it('should have hello world', () => {
-  render(<App />);
-  const message = screen.queryByText(/Hello World/i);
-  expect(message).toBeVisible();
-});
