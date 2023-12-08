6f82206f8aa09d1e04952927a1a1ee0801cf03e5
Minchuk Ilya <cacatubutyt@gmail.com>
2023-12-08 03:30:21 +0100

    feat: add empty main,sign in, sign up, welcome, not found pages

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,9 +4,24 @@ import {
   Route,
 } from 'react-router-dom';
 import RootLayout from '../layouts/RootLayout';
+import WelcomePage from '../pages/WelcomePage';
+import SignInPage from '../pages/SignInPage';
+import SignUpPage from '../pages/SignUpPage';
+import MainPage from '../pages/MainPage';
+import NotFoundPage from '../pages/NotFoundPage';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<RootLayout />}></Route>)
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<WelcomePage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sing-up" element={<SignUpPage />} />
+        <Route path="/main" element={<MainPage />} />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
 );
 
 export default router;
