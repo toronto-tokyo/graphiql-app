bc6ab0021a2da866a26c4bf129ca0da373161697
Minchuk Ilya <cacatubutyt@gmail.com>
2023-12-13 12:08:32 +0100

    feat: add locales constants data && locales data for header

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,13 +2,17 @@ import { NavLink, Outlet } from 'react-router-dom';
 import classes from './RootLayout.module.css';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
+import useRegion from '../hook/useRegion';
+import { LOCALE_DATA } from '../locales/constants/constants';
 
 function RootLayout() {
+  const region = useRegion();
+
   return (
     <div className={classes.wrapper}>
       <Header>
         <NavLink to="/" className={classes.link}>
-          Welcome
+          {region && LOCALE_DATA[region.region].header.link.welcome}
         </NavLink>
       </Header>
       <main className={classes.main}>
---
+++ src/locales/constants/constants.ts
@@ -0,0 +1,12 @@
+import enLocale from '../data/en.json';
+import deLocale from '../data/de.json';
+
+export const REGIONS = {
+  EN: 'EN',
+  DE: 'DE',
+};
+
+export const LOCALE_DATA = {
+  [REGIONS.EN]: enLocale,
+  [REGIONS.DE]: deLocale,
+};
