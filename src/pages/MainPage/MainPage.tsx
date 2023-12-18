c2765438ee3d90cb979950b5f254c614e16da53a
Minchuk Ilya <cacatubutyt@gmail.com>
2023-12-18 10:52:05 +0100

    feat: create fetchJSON function(based on createAsyncThunk) for implement fetching with redux

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,29 +1,13 @@
-import { useState } from 'react';
 import classes from './MainPage.module.css';
 import ApiLinkInput from '../../components/UI/ApiLinkInput/ApiLinkInput';
 import { useAppDispatch, useAppSelector } from '../../hook/useRedux';
-import { setQuery } from '../../redux/slices/GraphQLSlice';
+import { fetchJSON, setQuery } from '../../redux/slices/GraphQLSlice';
 
 function MainPage() {
-  const { apiLink, query } = useAppSelector((store) => store.graphQL);
+  const { apiLink, query, jsonViewer } = useAppSelector(
+    (store) => store.graphQL
+  );
   const dispatch = useAppDispatch();
-  const [jsonViewerData, setJsonViewerData] = useState('');
-
-  const searchHandle = async () => {
-    try {
-      const response = await fetch(apiLink, {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify({ query }),
-      });
-      const data = await response.json();
-      setJsonViewerData(JSON.stringify(data, null, 4));
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   return (
     <div className={classes.wrapper}>
@@ -39,14 +23,17 @@ function MainPage() {
           />
         </section>
         <section className={classes.controlPanel}>
-          <button onClick={searchHandle} disabled={!apiLink}>
+          <button
+            onClick={() => dispatch(fetchJSON({ url: apiLink, query }))}
+            disabled={!apiLink}
+          >
             Send
           </button>
         </section>
         <section className={classes.jsonViewerWrapper}>
           <textarea
             className={classes.jsonViewer}
-            value={jsonViewerData}
+            value={jsonViewer}
             readOnly
           />
         </section>
--- src/redux/slices/GraphQLSlice.ts
+++ src/redux/slices/GraphQLSlice.ts
@@ -1,15 +1,45 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { BASE_API_LINK, QUERY_TEMPLATE } from '../../shared/constants';
 
 type InitialState = {
   apiLink: string;
   query: string;
+  jsonViewer: string;
 };
 
 const initialState: InitialState = {
   apiLink: BASE_API_LINK,
   query: QUERY_TEMPLATE,
+  jsonViewer: '',
 };
+
+type FetchJSONParams = {
+  url: string;
+  query: string;
+};
+
+export const fetchJSON = createAsyncThunk<
+  string,
+  FetchJSONParams,
+  { rejectValue: string }
+>('GraphQLSlice/fetchJSON', async ({ url, query }, { rejectWithValue }) => {
+  try {
+    const request = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    });
+    if (!request.ok) {
+      rejectWithValue('Request Error!');
+    }
+    const data = await request.json();
+    return JSON.stringify(data, null, 4);
+  } catch (error) {
+    return rejectWithValue('Something went wrong!');
+  }
+});
 
 const GraphQLSlice = createSlice({
   name: 'GraphQL',
@@ -22,6 +52,11 @@ const GraphQLSlice = createSlice({
       console.log(action.payload);
       state.query = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchJSON.fulfilled, (state, action) => {
+      state.jsonViewer = action.payload;
+    });
   },
 });
 
