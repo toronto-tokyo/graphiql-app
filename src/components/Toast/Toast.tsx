458e58f7ad7608815c5926bd9cdab14750d8ed3a
Minchuk Ilya <cacatubutyt@gmail.com>
2023-12-18 18:57:51 +0100

    feat: create Toast component

---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react';
+import classes from './Toast.module.css';
+import CloseButton from '../UI/CloseButton/CloseButton';
+
+interface IProps {
+  children: ReactNode;
+  imgPath: string;
+  onClose: () => void;
+}
+
+const Toast = ({ children, imgPath, onClose }: IProps) => {
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.contentWrapper}>
+        <img className={classes.icon} src={imgPath} alt="toast-img" />
+        <span>{children}</span>
+      </div>
+      <CloseButton ariaLabel="Close" clickHandler={onClose} />
+    </div>
+  );
+};
+
+export default Toast;
