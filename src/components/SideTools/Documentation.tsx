085ec6803f285799f10e9222512f6e2b78016576
Minchuk Ilya <cacatubutyt@gmail.com>
2024-01-03 20:54:55 +0100

    feat: implement lazy-loading for Documentation component

--- src/components/SideTools/Documentation.tsx
+++ src/components/SideTools/Documentation.tsx
@@ -2,18 +2,28 @@ import classes from './Documentation.module.css';
 import useDocumentation from './useDocumentation';
 
 const Documentation = () => {
-  const { documentation, handleDocsBtnClick, docsBtnText } = useDocumentation();
+  const { documentation, handleDocsBtnClick, docsBtnText, isFetching } =
+    useDocumentation();
 
-  return (
-    <section className={classes.sideTools}>
-      <button onClick={handleDocsBtnClick}>{docsBtnText}</button>
-      <textarea
-        value={documentation}
-        className={classes.documentationContent}
-        readOnly
-      />
-    </section>
-  );
+  const renderContent = () => {
+    return isFetching ? (
+      <section className={classes.sideTools}>
+        <button onClick={handleDocsBtnClick} disabled>
+          {docsBtnText}
+        </button>
+      </section>
+    ) : (
+      <section className={classes.sideTools}>
+        <button onClick={handleDocsBtnClick}>{docsBtnText}</button>
+        <textarea
+          value={documentation}
+          className={classes.documentationContent}
+          readOnly
+        />
+      </section>
+    );
+  };
+  return renderContent();
 };
 
 export default Documentation;
--- src/components/SideTools/useDocumentation.ts
+++ src/components/SideTools/useDocumentation.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hook/useRedux';
 import useRegion from '../../hook/useRegion';
 import { LOCALE_DATA } from '../../locales/constants/constants';
@@ -6,8 +7,19 @@ import { fetchSchema } from '../../utils/fetchSchema';
 
 const useDocumentation = () => {
   const { documentation, apiLink } = useAppSelector((store) => store.graphQL);
+  const [isFetching, setIsFetching] = useState(true);
   const dispatch = useAppDispatch();
   const region = useRegion();
+
+  useEffect(() => {
+    (async () => {
+      setIsFetching(true);
+      const data = await fetchSchema({ url: apiLink });
+      const jsonData = JSON.stringify(data, null, 4);
+      dispatch(setDocumentation(jsonData));
+      setIsFetching(false);
+    })();
+  }, [apiLink, dispatch]);
 
   const handleDocsBtnClick = async () => {
     const data = await fetchSchema({ url: apiLink });
@@ -22,6 +34,7 @@ const useDocumentation = () => {
     documentation,
     handleDocsBtnClick,
     docsBtnText,
+    isFetching,
   };
 };
 
