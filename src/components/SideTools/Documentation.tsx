d42f2f173acd7be278aac3da924a1bb0d13514e9
Minchuk Ilya <cacatubutyt@gmail.com>
2024-01-03 21:26:16 +0100

    feat: split css styles of Documentation component in 2 column

--- src/components/SideTools/Documentation.tsx
+++ src/components/SideTools/Documentation.tsx
@@ -7,19 +7,25 @@ const Documentation = () => {
 
   const renderContent = () => {
     return isFetching ? (
-      <section className={classes.sideTools}>
-        <button onClick={handleDocsBtnClick} disabled>
-          {docsBtnText}
-        </button>
+      <section className={classes.wrapper}>
+        <div className={classes.documentationTools}>
+          <button onClick={handleDocsBtnClick} disabled>
+            {docsBtnText}
+          </button>
+        </div>
       </section>
     ) : (
-      <section className={classes.sideTools}>
-        <button onClick={handleDocsBtnClick}>{docsBtnText}</button>
-        <textarea
-          value={documentation}
-          className={classes.documentationContent}
-          readOnly
-        />
+      <section className={classes.wrapper}>
+        <div className={classes.documentationTools}>
+          <button onClick={handleDocsBtnClick}>{docsBtnText}</button>
+        </div>
+        <div className={classes.documentationContentWrap}>
+          <textarea
+            value={documentation}
+            className={classes.documentationContent}
+            readOnly
+          />
+        </div>
       </section>
     );
   };
