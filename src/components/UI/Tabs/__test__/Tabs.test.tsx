f0fffc70f49565c172b8dc51bb883afa5a4d6633
Minchuk Ilya <cacatubutyt@gmail.com>
2024-01-05 12:50:33 +0100

    test: add test for calling of function in Tabs component

--- src/components/UI/Tabs/__test__/Tabs.test.tsx
+++ src/components/UI/Tabs/__test__/Tabs.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { MockTabs } from './mockData';
 import Tabs from '../Tabs';
+import userEvent from '@testing-library/user-event';
 
 describe('Test Tabs component', () => {
   it('Component renders passed items count', () => {
@@ -18,5 +19,14 @@ describe('Test Tabs component', () => {
     expect(
       screen.getByText(MockTabs[0].label).className.includes('active')
     ).toBe(true);
+  });
+  it('Tabs component calls onClick function, when user clicks on tab', async () => {
+    const handleClick = vi.fn();
+    const user = userEvent.setup();
+    render(
+      <Tabs tabs={MockTabs} selectedId={MockTabs[0].id} onClick={handleClick} />
+    );
+    await user.click(screen.getByText(MockTabs[0].label));
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
