432e896781b32b752709e3580bda677a3536f8da
Minchuk Ilya <cacatubutyt@gmail.com>
2024-01-05 23:11:20 +0100

    test: add renderWithProvider utils function for tests

--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -1,6 +1,18 @@
 import { render, RenderResult } from '@testing-library/react';
 import { ReactNode } from 'react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import graphQLReducer from '../redux/slices/GraphQLSlice';
+
+export const renderWithProvider = (ui: ReactNode): RenderResult => {
+  const store = configureStore({
+    reducer: {
+      graphQL: graphQLReducer,
+    },
+  });
+  return render(<Provider store={store}>{ui}</Provider>);
+};
 
 export const renderWithRouter = (
   element: ReactNode,
