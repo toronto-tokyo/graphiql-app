f7eb577b5cb92471e3c359f5ebbb8f0b1ef10803
Minchuk Ilya <cacatubutyt@gmail.com>
2024-01-07 21:48:23 +0100

    feat: add ContentWapper component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import classes from './Footer.module.css';
 import { SchoolLink } from '../UI/SchoolLink/SchoolLink';
 import { GitHubLink } from '../UI/GitHubLink/GitHubLink';
+import ContentWrapper from '../UI/ContentWrapper/ContentWrapper';
 
 interface IProps {
   children?: ReactNode;
@@ -10,19 +11,23 @@ interface IProps {
 function Footer({ children }: IProps) {
   return (
     <footer className={classes.footer}>
-      {children}
-      <div className="developers">
-        <GitHubLink text="Ilya" href="https://github.com/toronto-tokyo" />
-        <GitHubLink text="Vladislav" href="https://github.com/GinezisNWD" />
-        <GitHubLink
-          text="Miroslav"
-          href="https://github.com/miroslav-zarenkov"
-        />
-      </div>
+      <ContentWrapper>
+        <div className={classes.innerContent}>
+          {children}
+          <div className="developers">
+            <GitHubLink text="Ilya" href="https://github.com/toronto-tokyo" />
+            <GitHubLink text="Vladislav" href="https://github.com/GinezisNWD" />
+            <GitHubLink
+              text="Miroslav"
+              href="https://github.com/miroslav-zarenkov"
+            />
+          </div>
 
-      <div>2024</div>
+          <div>2024</div>
 
-      <SchoolLink></SchoolLink>
+          <SchoolLink></SchoolLink>
+        </div>
+      </ContentWrapper>
     </footer>
   );
 }
