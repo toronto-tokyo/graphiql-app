f0c0c392f447c4cb11a8bc1219832cb0e857394b
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2024-01-07 17:16:54 +0400

    fix: add ru text to developers and fix github link image

--- src/components/Developers/Developers.tsx
+++ src/components/Developers/Developers.tsx
@@ -1,12 +1,16 @@
 import classes from './Developers.module.css';
-import { DEVELOPERS_DATA, DeveloperData } from '../../shared/developers-data';
 import { DeveloperCard } from '../DeveloperCard/DeveloperCard';
+import useRegion from '../../hook/useRegion';
+import { LOCALE_DATA } from '../../locales/constants/constants';
 
 export function Developers() {
-  const data = DEVELOPERS_DATA as DeveloperData[];
+  const region = useRegion();
+  const developersData =
+    (region && LOCALE_DATA[region.region].developers) ?? [];
+  const developersArray = Object.values(developersData);
   return (
     <div className={classes.developers}>
-      {data.map((elem, index) => (
+      {developersArray.map((elem, index) => (
         <DeveloperCard data={elem} key={index} />
       ))}
     </div>
