5e4b3fe1589c952bc935d6d621316753bd27db9f
Minchuk Ilya <cacatubutyt@gmail.com>
2024-01-19 19:44:18 +0100

    chore: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -25,4 +25,20 @@ export default defineConfig({
       ],
     },
   },
+  resolve: {
+    alias: {
+      assets: '/src/assets',
+      components: '/src/components',
+      hoc: '/src/hoc',
+      hook: '/src/hook',
+      layouts: '/src/layouts',
+      locales: '/src/locales',
+      pages: '/src/pages',
+      store: '/src/store',
+      router: '/src/router',
+      shared: '/src/shared',
+      test: '/src/test',
+      utils: '/src/utils',
+    }
+  }
 });
